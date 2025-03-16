fb477643f93034decb83d2560138d52d05e8cb85
Smolak1 <jcsmolakr@gmail.com>
2025-03-15 23:11:32 -0400

    Código semi-pronto

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 let nomeDoHeroi = "Junin Matador de porco"
-let xpDoHeroi = 0
+let xpDoHeroi = 100000000
 let classe
 if (xpDoHeroi < 1000) classe = "Ferro"
-    if else (xpDoHeroi >= 1000 && xpDoHeroi <= 2000) classe = "Bronze"
-        if else (xpDoHeroi >= 1000 && xpDoHeroi <= 2000) classe = "Prata"
-            if else (xpDoHeroi > 2000 && xpDoHeroi <= 5000) classe = "Ouro"
-                if else (xpDoHeroi >= 6000 && xpDoHeroi <= 7000) classe = "Prata"
-                     if else (xpDoHeroi >= 7000 && xpDoHeroi <= 8000) classe = "Prata"
-                         if else (xpDoHeroi >= 8000 && xpDoHeroi <= 9000) classe = "Prata"
-                            if else (xpDoHeroi >= 9000 && xpDoHeroi <= 10000) classe = "Prata"
-                                if else (xpDoHeroi > 10000) xpDoHeroi = "Radiante"
+    if  (xpDoHeroi >= 1000 && xpDoHeroi <= 2000) classe = "Bronze"
+            if  (xpDoHeroi > 2000 && xpDoHeroi <= 5000) classe = "Prata"
+                if (xpDoHeroi >= 6000 && xpDoHeroi <= 7000) classe = "Ouro"
+                     if (xpDoHeroi >= 7000 && xpDoHeroi <= 8000) classe = "Platina"
+                         if (xpDoHeroi >= 8000 && xpDoHeroi <= 9000) classe = "Ascendente"
+                             if (xpDoHeroi >= 9000 && xpDoHeroi <= 10000) classe = "Imortal"
+                                if (xpDoHeroi > 10000) classe = "Radiante"
 
 
 
