3b9012d09073519bf662a537f49e2febc121d214
Smolak1 <jcsmolakr@gmail.com>
2025-03-15 23:05:55 -0400

    primeira parte

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("bbbb")
+let nomeDoHeroi = "Junin Matador de porco"
+let xpDoHeroi = 0
+let classe
+if (xpDoHeroi < 1000) classe = "Ferro"
+    if else (xpDoHeroi >= 1000 && xpDoHeroi <= 2000) classe = "Bronze"
+        if else (xpDoHeroi >= 1000 && xpDoHeroi <= 2000) classe = "Prata"
+            if else (xpDoHeroi > 2000 && xpDoHeroi <= 5000) classe = "Ouro"
+                if else (xpDoHeroi >= 6000 && xpDoHeroi <= 7000) classe = "Prata"
+                     if else (xpDoHeroi >= 7000 && xpDoHeroi <= 8000) classe = "Prata"
+                         if else (xpDoHeroi >= 8000 && xpDoHeroi <= 9000) classe = "Prata"
+                            if else (xpDoHeroi >= 9000 && xpDoHeroi <= 10000) classe = "Prata"
+                                if else (xpDoHeroi > 10000) xpDoHeroi = "Radiante"
+
+
+
+                            console.log(classe)
