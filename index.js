9fe3c80f740a89f700113f163f990fd71d7606d9
Smolak1 <jcsmolakr@gmail.com>
2025-03-16 20:51:09 -0400

    código do projeto pronto

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-let nomeDoHeroi = "Junin Matador de porco"
-let xpDoHeroi = 100000000
-let classe
-if (xpDoHeroi < 1000) classe = "Ferro"
-    if  (xpDoHeroi >= 1000 && xpDoHeroi <= 2000) classe = "Bronze"
-            if  (xpDoHeroi > 2000 && xpDoHeroi <= 5000) classe = "Prata"
-                if (xpDoHeroi >= 6000 && xpDoHeroi <= 7000) classe = "Ouro"
-                     if (xpDoHeroi >= 7000 && xpDoHeroi <= 8000) classe = "Platina"
-                         if (xpDoHeroi >= 8000 && xpDoHeroi <= 9000) classe = "Ascendente"
-                             if (xpDoHeroi >= 9000 && xpDoHeroi <= 10000) classe = "Imortal"
-                                if (xpDoHeroi > 10000) classe = "Radiante"
+let nomeDoHeroi = "Junin Matador de porco";
+let xpDoHeroi = 0;
+let classe;
 
+if (xpDoHeroi < 1001) {
+    classe = "Ferro";
+} else if (xpDoHeroi <= 2000) {
+    classe = "Bronze";
+} else if (xpDoHeroi <= 5000) { // no código passado pelo felipe, 5500 FICARIA COMO NULL, JA QUE NÃO FOI ESPECIFICADO.
+    classe = "Prata";
+} else if (xpDoHeroi <= 7000) {
+    classe = "Ouro";
+} else if (xpDoHeroi <= 8000) {
+    classe = "Platina";
+} else if (xpDoHeroi <= 9000) {
+    classe = "Ascendente";
+} else if (xpDoHeroi <= 10000) {
+    classe = "Imortal";
+} else {
+    classe = "Radiante";
+}
 
-
-                            console.log(classe)
+console.log(classe);
